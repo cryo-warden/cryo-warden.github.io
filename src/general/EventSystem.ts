59bd063df636c50806d8d7276b5aee27a62a7b45
Cory <cryo.warden@gmail.com>
2023-10-15 22:55:23 -0700

    add flush and subscribeAll to EventSystem

--- src/general/EventSystem.ts
+++ src/general/EventSystem.ts
@@ -1,47 +1,70 @@
-type Event = {
+export type Event = {
   type: unknown;
 };
 
 type Type<E extends Event> = E["type"];
 
-type Listener<E extends Event, T extends Type<E>> = {
-  (event: Extract<E, { type: T }>): void;
-};
-
 export type Subscription = {
   cancel(): void;
 };
 
-type TypeSubscriptionListenerMap<E extends Event> = {
-  has<T extends Type<E>>(type: T): boolean;
-  get<T extends Type<E>>(type: T): Map<Subscription, Listener<E, T>>;
-  set<T extends Type<E>>(
+type Listener<TEvent extends Event, T extends Type<TEvent>> = {
+  (event: Extract<TEvent, { type: T }>): void;
+};
+
+export type Subscribe<TEvent extends Event> = {
+  <T extends Type<TEvent>>(
     type: T,
-    subscriptionListenerMap: Map<Subscription, Listener<E, T>>
-  ): void;
-  delete<T extends Type<E>>(type: T): void;
+    listener: Listener<TEvent, T>
+  ): Subscription;
 };
 
-export type Subscribe<E extends Event> = {
-  <T extends Type<E>>(type: T, listener: Listener<E, T>): Subscription;
+type ListenerAll<TEvent extends Event> = {
+  (event: TEvent): void;
 };
 
-export type Publish<E extends Event> = {
-  <T extends Type<E>>(event: Extract<E, { type: T }>): void;
+export type SubscribeAll<TEvent extends Event> = {
+  (listener: ListenerAll<TEvent>): Subscription;
 };
 
-export type EventSystem<E extends Event> = {
-  subscribe: Subscribe<E>;
-  publish: Publish<E>;
+export type Publish<TEvent extends Event> = {
+  <T extends Type<TEvent>>(event: Extract<TEvent, { type: T }>): void;
 };
 
 const emptySubscription = {
   cancel: () => {},
 };
 
-export const createEventSystem = <E extends Event>(): EventSystem<E> => {
-  const typeSubscriptionListenerMap =
-    new Map() as TypeSubscriptionListenerMap<E>;
+type TypeSubscriptionListenerMap<TEvent extends Event> = {
+  has<T extends Type<TEvent>>(type: T): boolean;
+  get<T extends Type<TEvent>>(type: T): Map<Subscription, Listener<TEvent, T>>;
+  set<T extends Type<TEvent>>(
+    type: T,
+    subscriptionListenerMap: Map<Subscription, Listener<TEvent, T>>
+  ): void;
+  delete<T extends Type<TEvent>>(type: T): void;
+};
+
+export type EventSystem<TEvent extends Event> = {
+  subscribe: Subscribe<TEvent>;
+  subscribeAll: SubscribeAll<TEvent>;
+  /** Queue an Event for publication on the next call to this EventSystem's flush method. */
+  publish: Publish<TEvent>;
+  /** Clear the queue of all Events, in the order of their original publication. */
+  flush: () => void;
+};
+
+export const createEventSystem = <
+  TEvent extends Event,
+>(): EventSystem<TEvent> => {
+  const subscriptionToAllEventTypesMap: Map<
+    Subscription,
+    ListenerAll<TEvent>
+  > = new Map();
+  const typeSubscriptionListenerMap: TypeSubscriptionListenerMap<TEvent> =
+    new Map();
+
+  const publicationQueue: (() => void)[] = [];
 
   return {
     subscribe: (type, listener) => {
@@ -68,14 +91,32 @@ export const createEventSystem = <E extends Event>(): EventSystem<E> => {
 
       return subscription;
     },
+    subscribeAll: (listener) => {
+      const subscription = {
+        cancel: () => {
+          subscriptionToAllEventTypesMap.delete(subscription);
+        },
+      };
+
+      subscriptionToAllEventTypesMap.set(subscription, listener);
+
+      return subscription;
+    },
     publish: (event) => {
-      const eventListeners = typeSubscriptionListenerMap.get(event.type);
+      publicationQueue.push(() => {
+        subscriptionToAllEventTypesMap.forEach((listener) => listener(event));
 
-      if (eventListeners == null) return;
+        const eventListeners = typeSubscriptionListenerMap.get(event.type);
 
-      eventListeners.forEach((listener) => {
-        listener(event);
+        if (eventListeners == null) return;
+
+        eventListeners.forEach((listener) => listener(event));
       });
+    },
+    flush: () => {
+      while (publicationQueue.length > 0) {
+        publicationQueue.shift()?.();
+      }
     },
   };
 };
